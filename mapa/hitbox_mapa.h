eca1fc07af2b42fa83634179f0fdfe454591b07b
Bernardo Cavalcanti Carneiro Leão <bernardocavalcanticarneiroleao@MacBook-Air-de-Bernardo.local>
2025-11-04 10:54:42 -0300

    criando hitbox do mapa.h para ele ler

---
+++ mapa/hitbox_mapa.h
@@ -0,0 +1,13 @@
+// mapa.h
+#ifndef MAPA_H
+#define MAPA_H
+
+#include "raylib.h"
+
+#define Tile_size 16
+#define Map_x (1280/Tile_size)
+#define Map_y (720/Tile_size)
+
+void carregar_mapa(int mapa[Map_y][Map_x]);
+
+#endif
